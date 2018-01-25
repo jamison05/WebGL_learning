c19358bdce25a319befc7f1f50c7a524f2faaa01
jamison05 <joshua.jamison2@gmail.com>
2018-01-24 22:53:37 -0500

    Working code
    gradient

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
+/**
+ * Part 2 Challenges:
+ *
+ * - Change the gradient to be white
+ *   to black, left-to-right only
+ * - Make a checkerboard pattern (hint: https://www.opengl.org/sdk/docs/man/html/mod.xhtml)
+ * - Make a checkerboard pattern without using conditions! (hint: https://www.opengl.org/sdk/docs/man/html/step.xhtml)
+ */
+
 var canvas = document.getElementById("main");
 var gl = canvas.getContext('webgl');
+
+/**
+ * Shaders
+ */
 
 // Utility to fail loudly on shader compilation failure
 function compileShader(shaderSource, shaderType) {
@@ -27,8 +40,9 @@ void main() {\n\
 var fragmentShader = compileShader(`\n\
 \n\
 void main(){\n\
-    // Draw every pixel red.\n\
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n\
+    gl_FragColor = vec4(gl_FragCoord.x/500.0, \n\
+                        gl_FragCoord.y/400.0, \n\
+                        0.0, 1.0);\n\
 }\n\
 `, gl.FRAGMENT_SHADER);
 
@@ -37,3 +51,62 @@ gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
 gl.linkProgram(program);
 gl.useProgram(program);
+
+/**
+ * Geometry setup
+ */
+
+// Set up 4 vertices, which we'll draw as a rectangle
+// via 2 triangles
+//
+//   A---C
+//   |  /|
+//   | / |
+//   |/  |
+//   B---D
+//
+// We order them like so, so that when we draw with
+// gl.TRIANGLE_STRIP, we draw triangle ABC and BCD.
+var vertexData = new Float32Array([
+    -1.0,  1.0, // top left
+    -1.0, -1.0, // bottom left
+     1.0,  1.0, // top right
+     1.0, -1.0, // bottom right
+]);
+var vertexDataBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+
+/**
+ * Attribute setup
+ */
+
+// Utility to complain loudly if we fail to find the attribute
+
+function getAttribLocation(program, name) {
+    var attributeLocation = gl.getAttribLocation(program, name);
+    if (attributeLocation === -1) {
+        throw 'Can not find attribute ' + name + '.';
+    }
+    return attributeLocation;
+}
+
+// To make the geometry information available in the shader as attributes, we
+// need to tell WebGL what the layout of our data in the vertex buffer is.
+var positionHandle = getAttribLocation(program, 'position');
+gl.enableVertexAttribArray(positionHandle);
+gl.vertexAttribPointer(positionHandle,
+                       2, // position is a vec2
+                       gl.FLOAT, // each component is a float
+                       gl.FALSE, // don't normalize values
+                       2 * 4, // two 4 byte float components per vertex
+                       0 // offset into each span of vertex data
+                       );
+
+/**
+ * Draw
+ */
+
+// Render the 4 vertices specified above (starting at index 0)
+// in TRIANGLE_STRIP mode.
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
